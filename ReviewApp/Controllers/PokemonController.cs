edadcd4c4ee8306b24a2bbcd82aa1b36619535ea
mfbernasol <mfbernasol@gmail.com>
2024-05-20 01:36:42 -0500

    Create GET and Read Methods

--- ReviewApp/Controllers/PokemonController.cs
+++ ReviewApp/Controllers/PokemonController.cs
@@ -1,5 +1,7 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using ReviewApp.Dto;
 using ReviewApp.Interfaces;
 using ReviewApp.Models;
 
@@ -9,17 +11,19 @@ namespace ReviewApp.Controllers;
 public class PokemonController : ControllerBase
 {
     private readonly IPokemonRepository _pokemonRepository;
+    private readonly IMapper _mapper;
 
-    public PokemonController(IPokemonRepository pokemonRepository)
+    public PokemonController(IPokemonRepository pokemonRepository, IMapper mapper)
     {
         _pokemonRepository = pokemonRepository;
+        _mapper = mapper;
     }
 
     [HttpGet]
     [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
     public IActionResult GetPokemons()
     {
-        var pokemons= _pokemonRepository.GetPokemons();
+        var pokemons =  _mapper.Map<List<PokemonDto>> (_pokemonRepository.GetPokemons());
 
         if (!ModelState.IsValid)
         {
@@ -27,5 +31,40 @@ public class PokemonController : ControllerBase
         }
 
         return Ok(pokemons);
+    }
+
+    [HttpGet("{pokeId}")]
+    [ProducesResponseType(200, Type = typeof(Pokemon))]
+    [ProducesResponseType(400)]
+    public IActionResult GetPokemon(int pokeId)
+    {
+        if (!_pokemonRepository.PokemonExists(pokeId))
+            return NotFound();
+
+        var pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(pokeId));
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(pokemon);
+        
+    }
+
+    [HttpGet("{pokeId}/rating")]
+    [ProducesResponseType(200, Type = typeof(Pokemon))]
+    [ProducesResponseType(400)]
+    public IActionResult GetPokemonRating(int pokeId)
+    {
+        if (!_pokemonRepository.PokemonExists(pokeId))
+        {
+            return NotFound();
+        }
+
+        var rating = _pokemonRepository.GetPokemonRating(pokeId);
+
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        return Ok(rating);
     }
 }
